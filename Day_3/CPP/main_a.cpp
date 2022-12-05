3c67c71937c42573800501ac1b41c838877ce645
Luis Serrano Hernández <luiserh1@inf.upv.es>
2022-12-05 19:02:53 +0100

    Day 3. Part A. C++

--- Day_3/CPP/main_a.cpp
+++ Day_3/CPP/main_a.cpp
@@ -1,6 +1,10 @@
+//#define VERBOSE
+
 #include <iostream>		// Salida por consola
 #include <fstream>		// Lectura de archivo de input
 #include <string>		// std::string
+#include <map>			// std::map
+#include <set>			// std::set
 #include <filesystem>	// Rutas canónicas
 
 int main()
@@ -27,11 +31,69 @@ int main()
 	}
 	
 	std::string line;
+	int prioritySum = 0;
+	// Loopinng all over the file
 	while (std::getline(file, line))
 	{
-		std::cout << line << std::endl;
+		// Format check
+		if (line.empty() || line.size() % 2 != 0)
+		{
+			std::cout << "Error: La línea no está bien formateada\n";
+			return 3;			
+		}
+		
+		// Searching for the repeated char
+		size_t bottomIndex = 0;
+		size_t upperIndex  = line.size() - 1;
+		
+		char repeatedChar = '\0';
+		std::set<char> lSet, rSet;
+		for (size_t i = 0; i < line.size() / 2; i++)
+		{
+			char leftChar  = line[bottomIndex + i];
+			if (rSet.find(leftChar) != rSet.end()) 
+			{
+				repeatedChar = leftChar;
+				break;
+			}
+			lSet.emplace(leftChar);
+				
+			char rightChar = line[upperIndex - i];
+			if (lSet.find(rightChar) != lSet.end()) 
+			{
+				repeatedChar = rightChar;
+				break;
+			}
+			rSet.emplace(rightChar);
+		}
+
+		// Getting the priority
+		int linePriority = 0;
+		
+		int repeatedCharASCIIInndex = static_cast<int>(repeatedChar);
+		if (repeatedCharASCIIInndex > 64 && repeatedCharASCIIInndex < 91)		// A..Z
+		{
+			// 27..52
+			linePriority = repeatedCharASCIIInndex - 64 + 26;
+		}
+		else if (repeatedCharASCIIInndex > 96 && repeatedCharASCIIInndex < 123)	// a..z
+		{
+			// 1..26
+			linePriority = repeatedCharASCIIInndex - 96;
+		}
+		else //if (!repeatedChar)
+		{
+			std::cout << "Error: La línea no está bien formateada. EL caracter \"" << repeatedChar << "\" no es válido";
+			return 4;	
+		}
+#ifdef VERBOSE
+		std::cout << line << ": " << repeatedChar << "(" << linePriority << ")" << std::endl;
+#endif
+		prioritySum += linePriority;
 	}
 	file.close();
+	
+	std::cout << "The total priority sum is: " << "\033[33m" << prioritySum << "\033[m" << std::endl;
 	
 	std::system("pause");
 	return 0;
